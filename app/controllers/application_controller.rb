895d6759f285e5f8316eaf1836420cb0907ae88c
nathan-stamand <nathansaint810@gmail.com>
2020-04-30 14:02:12 -0500

    added homepage controller and helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,21 @@ class ApplicationController < Sinatra::Base
         enable :sessions 
         set :session_secret, 'secret'
     end
+
+    get '/' do 
+        erb :index
+    end
+
+    helpers do 
+
+        def logged_in? 
+            !!session[:user_id]
+        end
+
+        def current_user 
+            User.find_by(id: session[:user_id])
+        end
+
+    end
+    
 end
