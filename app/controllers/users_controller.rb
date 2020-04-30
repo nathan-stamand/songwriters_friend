472addbdcce53fa1b12e882ec3ece87e2d29e325
nathan-stamand <nathansaint810@gmail.com>
2020-04-30 14:09:43 -0500

    added empty controllers for create, read, update, and delete functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,39 @@
 class UsersController < ApplicationController
 
+    get "/users/signup" do
+
+    end
+
+    post "/users/signup" do 
+
+    end 
+
+    get "/users/login" do 
+
+    end 
+
+    post "/users/login" do 
+
+    end
+
+    get "/users/logout" do 
+
+    end
+
+    get "/users/:id" do 
+
+    end
+
+    post "/users/:id" do 
+
+    end 
+
+    patch "/users/:id/edit" do 
+
+    end
+
+    delete "/users/:id/delete" do 
+
+    end
+
 end
