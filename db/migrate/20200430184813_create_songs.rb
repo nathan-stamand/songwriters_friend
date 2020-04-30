15a313158c06eb4ce431b5f71ec1c33d2996c757
nathan-stamand <nathansaint810@gmail.com>
2020-04-30 13:58:14 -0500

    created migrations to create users table and songs table w/ columns

---
+++ db/migrate/20200430184813_create_songs.rb
@@ -0,0 +1,15 @@
+class CreateSongs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :songs do |s|
+      s.string :title 
+      s.string :lyrics 
+      s.string :chords
+      s.string :tempo 
+      s.string :key_signature 
+      s.string :time_signature
+      s.string :date_created 
+      s.string :co_authors
+      s.integer :user_id
+    end 
+  end
+end
