83a034bea188182e7bfe1ab7176ea9a7f6374bd7
nathan-stamand <nathansaint810@gmail.com>
2020-05-01 15:36:55 -0500

    added variables for params in patch/edit command, updates song with new info and redirects to show page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,7 +27,7 @@ class SongsController < ApplicationController
         time = Time.new
         @date = time.ctime
 
-        @song = Song.create(title: @title, tempo: @tempo, time_signature: @time_sig, key_signature: @key_sig, co_authors: @co_authors, lyrics: @lyrics, date_created: @date)
+        @song = Song.create(title: @title, tempo: @tempo, time_signature: @time_sig, key_signature: @key_sig, co_authors: @co_authors, lyrics: @lyrics)
         @user.songs << @song 
         @user.save
         
@@ -55,9 +55,27 @@ class SongsController < ApplicationController
         end
     end
 
-    patch "songs/:id/edit" do 
+    patch "/songs/:id/edit" do 
         @song = Song.find_by(id: params[:id])
+        if !logged_in? || current_user.id != @song.user.id
+            redirect to "/users/login"
+        end 
+        @title = params[:title]
+        @tempo = params[:tempo][:estimate_tempo]
+        exact_tempo = params[:tempo][:exact_tempo]
+
+        if @tempo.length == 0
+            @tempo = exact_tempo 
+        end
+        @time_sig = params[:time_signature]
+        @key_sig = params[:key_signature][:letter] + params[:key_signature][:sharp_flat] + ' ' + params[:key_signature][:maj_min]
+        @co_authors = params[:co_authors]
+        @lyrics = params[:lyrics]
+
+        @song.update(title: @title, tempo: @tempo, time_signature: @time_sig, key_signature: @key_sig, co_authors: @co_authors, lyrics: @lyrics)
+        @song.save
         
+        redirect to "/songs/#{@song.id}"
     end
 
 end
