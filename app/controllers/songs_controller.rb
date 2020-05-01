1e951ef8316555e2be4a2b3411a8bad229563ccf
nathan-stamand <nathansaint810@gmail.com>
2020-05-01 11:09:04 -0500

    created variables for song, create song, and assignment to artist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,4 +6,32 @@ class SongsController < ApplicationController
         end
         erb :"songs/new"
     end
+
+    post "/songs/new" do 
+        if !logged_in?
+            redirect to "/users/login"
+        end
+        @user = current_user 
+        @title = params[:title]
+        @tempo = params[:tempo][:estimate_tempo]
+        exact_tempo = params[:tempo][:exact_tempo]
+
+        if @tempo.length == 0
+            @tempo = exact_tempo 
+        end
+
+        @time_sig = params[:time_signature]
+        @key_sig = params[:key_signature][:letter] + params[:key_signature][:sharp_flat] + ' ' + params[:key_signature][:maj_min]
+        @co_authors = params[:co_authors]
+        @lyrics = params[:lyrics].gsub("\r\n", ' / ')
+        time = Time.new
+        @date = time.ctime
+
+        @song = Song.create(title: @title, tempo: @tempo, time_signature: @time_sig, key_signature: @key_sig, co_authors: @co_authors, lyrics: @lyrics, date_created: @date)
+        @user.songs << @song 
+        @user.save
+        
+        
+    end
+
 end
