71f1038e61bf3101841b3741669123cb18da2a9b
nathan-stamand <nathansaint810@gmail.com>
2020-05-01 08:14:05 -0500

    added login check and render form

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,9 @@
 class SongsController < ApplicationController
 
+    get "/songs/new" do 
+        if !logged_in? 
+            redirect to "users/login"
+        end
+        erb :"songs/new"
+    end
 end
