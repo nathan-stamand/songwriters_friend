f3ceea72df7bea29d6d69effa17af1ca7dc08f3f
nathan-stamand <nathansaint810@gmail.com>
2020-05-01 16:21:40 -0500

    fixed date_created issue, added param back to song's creation

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,7 +27,7 @@ class SongsController < ApplicationController
         time = Time.new
         @date = time.ctime
 
-        @song = Song.create(title: @title, tempo: @tempo, time_signature: @time_sig, key_signature: @key_sig, co_authors: @co_authors, lyrics: @lyrics)
+        @song = Song.create(title: @title, tempo: @tempo, time_signature: @time_sig, key_signature: @key_sig, co_authors: @co_authors, lyrics: @lyrics, date_created: @date)
         @user.songs << @song 
         @user.save
         
